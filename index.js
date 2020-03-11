c9e26ab2d7c97d74ad96c2fdf4608f1f9aeee90e
Matthew Ewan-Laws <matt.ewan-laws@hrgo.co.uk>
2020-03-11 17:14:36 +0000

    get path right

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const fs = require("fs");
 const yaml = require("yaml");
 
-const namesYaml = fs.readFileSync("./possible-names.yaml", "utf8");
+const dir = __dirname;
+const namesYaml = fs.readFileSync(`${dirname}/possible-names.yaml`, "utf8");
 const names = yaml.parse(namesYaml);
 
 const nameIdx = Math.floor(Math.random() * names.length);
