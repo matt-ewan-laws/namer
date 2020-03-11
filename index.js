fd04c6b053cccaf883dbddf98066bf32bb0c0821
Matthew Ewan-Laws <matt.ewan-laws@hrgo.co.uk>
2020-03-11 17:15:59 +0000

    fix dirname

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const yaml = require("yaml");
 
 const dir = __dirname;
-const namesYaml = fs.readFileSync(`${dirname}/possible-names.yaml`, "utf8");
+const namesYaml = fs.readFileSync(`${dir}/possible-names.yaml`, "utf8");
 const names = yaml.parse(namesYaml);
 
 const nameIdx = Math.floor(Math.random() * names.length);
